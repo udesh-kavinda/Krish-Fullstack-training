7388bcc4bfb919324907b0d2927454bf2526ff2a
udesh-kavinda <90600874+udesh-kavinda@users.noreply.github.com>
2021-09-29 00:32:49 +0530

    Big desimal lib

--- string reverse/stringreverse.java
+++ string reverse/stringreverse.java
@@ -1,4 +1,6 @@
-public static void main (String args[]){
+public class stringreverse {
+
+        public static void main (String args[]){
 
             String sentence = "apple";
 
@@ -6,16 +8,12 @@ public static void main (String args[]){
             char c[] =sentence.toCharArray();
             char r[] = new char[length];
 
-            System.out.println(length);
-
             for(int i=length;i>=1;i--){
-
                 r[i-1]=c[(length)-i];
-
-
             }
+
             String reverse = new String(r);
-            //reverse = Arrays.toString(r);
             System.out.println(reverse);
 
         }
+    }
